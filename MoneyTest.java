ce01e30b242e4d716a99c65d8c4d3fc42e2b836c
kayrayokoyama <yokoyamakayra03@gmail.com>
2026-09-23 22:28:08 -0300

    currency

--- MoneyTest.java
+++ MoneyTest.java
@@ -6,7 +6,7 @@ import java.beans.Transient;
 public class MoneyTest {
     @Test 
     public void testMultiplication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(Money.dollar (10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
@@ -26,19 +26,35 @@ public class MoneyTest {
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
+
+    @Test 
+    public void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
 
-class Money {
+abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     abstract Money times(int multiplier);
 
+    String currency() {
+        return currency;
+    }
+
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     @Override 
@@ -49,21 +65,21 @@ class Money {
 }
 
 class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
-    Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);
+    Money times(int multiplier) {
+        return Money.dollar(amount * multiplier);
     }
 }
 
 class Franc extends Money {
-    Fran(int amount) {
-        this.amount = amount;
+    Fran(int amount, String currency) {
+        super(amount, currency);
     }
 
     Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 }
